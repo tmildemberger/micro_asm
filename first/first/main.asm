e9948ecc5e080ce7bc37c33f52ecaf172d5f4e37
Jake Snowy <thimil7@gmail.com>
2018-04-05 10:49:40 -0300

    ATMega first commit

---
+++ first/first/main.asm
@@ -0,0 +1,43 @@
+;
+; first.asm
+;
+; Created: 29/03/2018 16:13:04
+; Author : THIAGO
+; Version: v1.0.0 (29-03-2018-16:13:04)
+
+.equ CONTAGEM1 = 0xCA
+.equ CONTAGEM2 = 0xA4
+
+.org 0x0000
+	rjmp inicio
+
+; Replace with your application code
+.org 0x0040
+inicio:
+	ldi r18, 0x08
+	out sph, r18
+	ldi r18, 0x00
+	out spl, r18
+
+program:
+	inc r16
+	;call other_function(maybe with LEDs)
+    call timer_100ms
+	dec r16
+    rjmp program
+
+
+.org 0x0400
+timer_100ms:
+	nop
+	nop
+	ldi r22, CONTAGEM1
+outer_loop:
+		ldi r21, CONTAGEM2
+inner_loop:
+			dec r21
+			brne inner_loop
+		dec r22
+		brne outer_loop
+	ret
+	
